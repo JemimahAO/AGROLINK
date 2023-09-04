4bc3b118dea766ffa61ed91e419ee3986d893dcb
Jinx <jemimahadwar@gmail.com>
2023-09-04 10:31:37 +0300

    changes for splash

--- PROJECT_AGROLINK/PROJECT_AGROLINK/settings.py
+++ PROJECT_AGROLINK/PROJECT_AGROLINK/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'splash.apps.GuestuserConfig',
+    'splash.apps.SplashConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PROJECT_AGROLINK/splash/apps.py
+++ PROJECT_AGROLINK/splash/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GuestuserConfig(AppConfig):
+class SplashConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'splash'
